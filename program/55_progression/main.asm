dad22529317023e51a27b859b0c6c8dd01af8bf5
leventium <leventium@users.noreply.github.com>
2023-10-09 17:18:26 +0300

    Add gpio support 55_progression

--- program/55_progression/main.asm
+++ program/55_progression/main.asm
@@ -1,7 +1,11 @@
            .text
 
-start:     addiu $t1, $zero, 13
-           addiu $t2, $zero, 91
+start:     li $t7, 0xbebe
+           sw $t7, 0x1001beb0
+           lw $t1, 0x1001beb4
+           li $t7, 0xbaba
+           sw $t7, 0x1001beb0
+           lw $t2, 0x1001beb4
            addu $t3, $t1, $t2
            subu $t4, $t2, $t1
            addiu $t4, $t4, 2
@@ -9,5 +13,6 @@ start:     addiu $t1, $zero, 13
            
            addiu $t6, $zero, 2
            srlv $v0, $t5, $t6
+           sw $v0, 0x1001beb0
            
 end:       beq $zero, $zero, end
