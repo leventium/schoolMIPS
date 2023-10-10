8ad93a8e095a139eb9b42f7c296b48aec4d75d7a
leventium <leventium@users.noreply.github.com>
2023-10-10 12:55:46 +0300

    Modificate test program

--- program/77_gpio_test/main.asm
+++ program/77_gpio_test/main.asm
@@ -1,8 +1,4 @@
 	.text
-start:  addiu $v1, $zero, 10
-	sw $v1, 4
-	lw $v0, 4
-	lw $t0, 0x1001beb4
-	addu $v0, $v0, $t0
-	sw $v0, 0x1001beb0
+start:  lw $t0, 0x1001beb4
+	sw $t0, 0x1001beb0
 end:	beqz $zero, end
