cc254cf0cebdc6039f794f302194532b73314433
leventium <leventium@users.noreply.github.com>
2023-11-07 12:46:54 +0300

    Fix decoder error

--- src_simple_mod/decoder.v
+++ src_simple_mod/decoder.v
@@ -1,7 +1,7 @@
 module decoder (
-    input clk,
-    input key[2:0],
-    output out[7:0]
+    input        clk,
+    input  [2:0] key,
+    output [7:0] out
 );
     always @(posedge clk)
         out <= 1'b1 << key;
