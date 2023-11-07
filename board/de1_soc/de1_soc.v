25ba9e248b451bc69536f467544e668cc8e05eec
leventium <leventium@users.noreply.github.com>
2023-11-07 12:21:23 +0300

    Add decoder usement in board verilog

--- board/de1_soc/de1_soc.v
+++ board/de1_soc/de1_soc.v
@@ -188,39 +188,11 @@ module de1_soc(
       input       [9:0]  SW
       `endif
 );
-
-    // wires & inputs
-    wire          clk;
-    wire          clkIn     =  CLOCK_50;
-    wire          rst_n     =  KEY[0];
-    wire          clkEnable =  SW [9] | ~KEY[1];
-    wire [  3:0 ] clkDevide =  SW [8:5];
-    wire [  4:0 ] regAddr   =  SW [4:0];
-    wire [ 31:0 ] regData;
-
-    //cores
-    sm_top sm_top
+    decoder dec
     (
-        .clkIn      ( clkIn     ),
-        .rst_n      ( rst_n     ),
-        .clkDevide  ( clkDevide ),
-        .clkEnable  ( clkEnable ),
-        .clk        ( clk       ),
-        .regAddr    ( regAddr   ),
-        .regData    ( regData   )
+        .clk  ( KEY  [0]   ),
+        .key  ( SW   [2:0] ),
+        .out  ( LEDR [7:0] )
     );
-
-    //outputs
-    assign LEDR[0]   = clk;
-    assign LEDR[9:1] = regData[8:0];
-
-    wire [ 31:0 ] h7segment = regData;
-
-    sm_hex_display digit_5 ( h7segment [23:20] , HEX5 [6:0] );
-    sm_hex_display digit_4 ( h7segment [19:16] , HEX4 [6:0] );
-    sm_hex_display digit_3 ( h7segment [15:12] , HEX3 [6:0] );
-    sm_hex_display digit_2 ( h7segment [11: 8] , HEX2 [6:0] );
-    sm_hex_display digit_1 ( h7segment [ 7: 4] , HEX1 [6:0] );
-    sm_hex_display digit_0 ( h7segment [ 3: 0] , HEX0 [6:0] );
 
 endmodule
